c6117198b804968c568cbe3e3f72dfc90e618163
Sean Lynch <sean.lynch.sl1@contractors.roche.com>
2020-11-18 10:03:33 -0800

    First working version

--- eks_crutch/cli.py
+++ eks_crutch/cli.py
@@ -1,18 +1,55 @@
 """Console script for eks_crutch."""
 import argparse
+import getpass
+import socket
+import os
 import sys
+
+from . import eks_crutch
 
 
 def main():
     """Console script for eks_crutch."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
+    parser.add_argument(
+        '--token-path',
+        default='/var/run/secrets/eks.amazonaws.com/serviceaccount/token',
+        help='Path to read OIDC token from'
+    )
+    parser.add_argument(
+        '--session-name',
+        default=None,
+        help='Session name to use for AssumeRoleWithWebIdentity call'
+    )
+    parser.add_argument(
+        '--duration',
+        default=3600,
+        help='Session duration in seconds'
+    )
+    parser.add_argument(
+        '--role-arn',
+        help='ARN of role to assume'
+    )
+    parser.add_argument('command')
+    parser.add_argument('args', nargs='*')
     args = parser.parse_args()
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "eks_crutch.cli.main")
-    return 0
+    session_name = args.session_name
+    if session_name is None:
+        user = getpass.getuser()
+        hostname = socket.gethostname()
+        session_name = '{}@{}'.format(user, hostname)
+
+    token = eks_crutch.read_token(args.token_path)
+    credentials = eks_crutch.assume_role(
+        role_arn=args.role_arn,
+        session_name=session_name,
+        token=token,
+        duration=args.duration
+    )
+    env = eks_crutch.get_environment(credentials)
+    os.environ.update(env)
+    os.execvp(args.command, [args.command] + args.args)
 
 
 if __name__ == "__main__":
--- eks_crutch/eks_crutch.py
+++ eks_crutch/eks_crutch.py
@@ -1,1 +1,42 @@
 """Main module."""
+
+import os
+from typing import TypedDict
+
+import boto3
+
+
+class Credentials(TypedDict):
+    AccessKeyId: str
+    SecretAccessKey: str
+    SessionToken: str
+
+
+def assume_role(role_arn: str, session_name: str, token: str, duration: int = 3600) -> Credentials:
+    sts = boto3.client('sts')
+    r = sts.assume_role_with_web_identity(
+        RoleArn = role_arn,
+        RoleSessionName = session_name,
+        WebIdentityToken = token)
+
+    c = r['Credentials']
+    return {
+        'AccessKeyId': c['AccessKeyId'],
+        'SecretAccessKey': c['SecretAccessKey'],
+        'SessionToken': c['SessionToken']
+    }
+
+
+def read_token(path: os.PathLike = '/var/run/secrets/eks.amazonaws.com/serviceaccount/token') -> str:
+    with open(path, 'r') as fp:
+        token = fp.read()
+
+    return token
+
+
+def get_environment(credentials: Credentials):
+    return {
+        'AWS_ACCESS_KEY_ID': credentials['AccessKeyId'],
+        'AWS_SECRET_ACCESS_KEY': credentials['SecretAccessKey'],
+        'AWS_SESSION_TOKEN': credentials['SessionToken']
+    }
